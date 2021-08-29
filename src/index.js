d237c91d3c1f9eecf4c51a18264ae51c42055cd1
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-28 22:11:34 -0500

    binding store into main index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from "./store";
 import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+};
+registerServiceWorker();
+
+render();
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
