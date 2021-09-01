0502ebed9b920302bec941dbbaa0ee1fbf784cbd
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-31 21:11:55 -0500

    adding activeUserId reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import user from './user';
 import contacts from './contacts';
+import activeUserId from './activeUserID';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     user,
     contacts,
+    activeUserId
 });
