74ed3243438e6060164be91bc5f4bfc78d7ae0c0
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-31 23:24:52 -0500

    addin header component

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import store from '../store';
+import Header from '../components/Header';
 
 export default function ChatWindow({ activeUserId }) {
+    const state = store.getState();
+    const activeUser = state.contacts[activeUserId];
     return (
-        <div className="ChatWindow">Conversation for user id:
-            {activeUserId}
+        <div className="ChatWindow">
+            <Header user={activeUser} />
         </div>
     )
 }
