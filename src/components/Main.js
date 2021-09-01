d1e8ec67a2ff79d1a719e1d4a12df2c42f4fd799
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-31 21:20:47 -0500

    updating main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import "./Main.css";
+import Empty from "../components/Empty";
+import ChatWindow from './components/ChatWindow';
 
-export default function Main() {
-    return (
-        <main className="Main">Main Component</main>
-    )
+export default function Main({ user, activeUserId }) {
+    const renderMainContent = () => {
+        if (!activeUserId) {
+            return <Empty user={user} activeUserId={activeUserId} />;
+        } else {
+            return <ChatWindow activeUserId={activeUserId} />;
+        }
+    };
+    return <main className="Main">{renderMainContent()}</main>
 }
