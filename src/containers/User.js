4ceb6ed64dda04c8619b5d6d9b09a3b4239b5d21
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-31 22:14:16 -0500

    activeuser reducer

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import store from '../store';
 import "./User.css";
+import { setActiveUserId } from '../actions';
 
 export default function User({ user }) {
     const { name, profile_pic, status } = user;
     return (
-        <div className="User">
+        <div className="User" onClick={handleUserClick.bind(null, user)}>
             <img src={profile_pic} alt={name} className="User__pic" />
             <div className="User__details">
                 <p className="User__details-name">{name}</p>
@@ -12,4 +14,8 @@ export default function User({ user }) {
             </div>
         </div>
     )
+};
+
+function handleUserClick({user_id}) {
+    store.dispatch(setActiveUserId(user_id));
 }
