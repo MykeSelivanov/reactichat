29473075144d50f6af9fd25756883d19256c44cd
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-01 21:40:14 -0500

    adding messages reducer

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,5 +1,6 @@
 import { contacts } from '../data/static-data';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = contacts, action) => {
     return state;
 };
