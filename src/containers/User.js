f9d4f3eefd1ad5090bc76bb8b960b878569220ad
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-02 22:36:15 -0500

    adding x button

--- src/containers/User.js
+++ src/containers/User.js
@@ -7,16 +7,21 @@ export default function User({ user }) {
     const { name, profile_pic, status } = user;
     return (
         <div className="User" onClick={handleUserClick.bind(null, user)}>
+            <p className="User__closeIcon" onClick={(e) => handleCrossClick(e, user.user_id)}> x </p>
             <img src={profile_pic} alt={name} className="User__pic" />
             <div className="User__details">
                 <p className="User__details-name">{name}</p>
-                <button className="delete-user-button">Delete chat</button>
                 <p className="User__details-status">{status}</p>
             </div>
         </div>
     )
 };
 
-function handleUserClick({user_id}) {
+function handleUserClick({ user_id }) {
     store.dispatch(setActiveUserId(user_id));
+}
+
+function handleCrossClick(e, user_id) {
+    e.stopPropagation();
+
 }
