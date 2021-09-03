e1949dede2c38761d3f10eeaf97388d61a4d0990
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-02 22:42:04 -0500

    adding delete user reducer

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,6 +1,15 @@
-import { contacts } from '../data/static-data';
+import { contacts as userContancts } from '../data/static-data';
+import { DELETE_CONTACT } from '../actions/constants/action-types';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = contacts, action) => {
-    return state;
+export default function contacts(state = userContancts, action) {
+    switch (action.type) {
+        case DELETE_CONTACT:
+            const contactId = action.payload;
+            const { [contactId]: deleted, ...newState } = state;
+            return newState;
+
+        default:
+            return state;
+    }
 };
