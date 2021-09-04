89d775ebde28203b4c50377e88d65bef0e5d632f
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-03 22:17:16 -0500

    updating contacts reducer

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,14 +1,12 @@
 import { contacts as userContancts } from '../data/static-data';
 import { DELETE_CONTACT } from '../actions/constants/action-types';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default function contacts(state = userContancts, action) {
     switch (action.type) {
         case DELETE_CONTACT:
             const contactId = action.payload;
             const { [contactId]: deleted, ...newState } = state;
             return newState;
-
         default:
             return state;
     }
